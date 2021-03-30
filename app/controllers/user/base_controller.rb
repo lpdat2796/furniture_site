0f68f0585e8474058ed42c51063fa238d3d64aa7
lpdat2796 <lpdat2796@gmail.com>
2021-03-30 22:02:56 +0700

    feat: add dialog login

--- app/controllers/user/base_controller.rb
+++ app/controllers/user/base_controller.rb
@@ -2,12 +2,17 @@ class User::BaseController < ApplicationController
   layout 'application_user'
   helper_method :current_user
   helper_method :logged_in?
+  before_action :set_user
 
-  def current_user    
+  def current_user
     User.find_by(id: session[:user_id])  
   end
 
   def logged_in?
     !current_user.nil?  
+  end
+
+  def set_user
+    @user = User.new if session[:user_id].nil?
   end
 end
