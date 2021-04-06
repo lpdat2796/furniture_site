9194cb2c723bb6990133263d8539173300ee5189
lpdat2796 <lpdat2796@gmail.com>
2021-04-06 16:20:22 +0700

    feat: modify dashboard admin page

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,4 +1,7 @@
 class Admin::HomeController < Admin::BaseController
   def index
+    @orders = Order.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
+    @users = User.where(role: 'user')
+    @products = Product.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
   end
 end
