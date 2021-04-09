cc3b7ec88780102cb63c41d55480abac524f31eb
lpdat2796 <lpdat2796@gmail.com>
2021-04-09 15:54:33 +0700

    feat: add dynamic data for chart admin dashboard

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -3,5 +3,18 @@ class Admin::HomeController < Admin::BaseController
     @orders = Order.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
     @users = User.where(role: 'user')
     @products = Product.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
+    furniture_orders = OrderDetail.joins(product: :categories).where(categories: { name: 'Furniture', parent_id: 0 },
+      created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month).count
+    baby_furniture_orders = OrderDetail.joins(product: :categories).where(categories: { name: 'Baby furniture', parent_id: 0 },
+      created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month).count
+    decorator = OrderDetail.joins(product: :categories).where(categories: { name: 'Decoration', parent_id: 0 },
+      created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month).count
+    @arr_amount_orders = [furniture_orders, baby_furniture_orders, decorator]
+
+    @arr_total_amounts = []
+
+    for i in 1..12
+      @arr_total_amounts << Order.where('extract(month from created_at) = ?', i).pluck(:total_amount).inject { |sum, item| sum.to_i + item.to_i }.to_i
+    end
   end
 end
